c7078cc4abedc3ab2687724ef448e3e1568d6b0d
Brendon Walsh <brendonwalsh@niamu.com>
2018-12-27 00:47:03 -0500

    Extract colours of discs

---
+++ src/crokinole_score_keeper/color.clj
@@ -0,0 +1,47 @@
+(ns crokinole-score-keeper.color)
+
+(defn- pow [x n] (Math/pow x n))
+
+(defn- color-diff
+  "Calculate the distance between two colors"
+  [[r1 g1 b1] [r2 g2 b2]]
+  (Math/sqrt (+ (pow (- r1 r2) 2)
+                (pow (- g1 g2) 2)
+                (pow (- b1 b2) 2))))
+
+(defn avg
+  [colors]
+  (mapv #(Math/sqrt %)
+        [(/ (reduce + (map (fn [[r _ _]] (pow r 2)) colors)) (count colors))
+         (/ (reduce + (map (fn [[_ g _]] (pow g 2)) colors)) (count colors))
+         (/ (reduce + (map (fn [[_ _ b]] (pow b 2)) colors)) (count colors))]))
+
+(defn k-means
+  [coll centroids distance-fn avg-fn iterations]
+  (let [result (reduce (fn [accl color]
+                         (let [[centroid distance]
+                               (->> centroids
+                                    (map #(vector % (distance-fn % color)))
+                                    (sort-by last)
+                                    first)]
+                           (update accl centroid conj color)))
+                       (apply hash-map
+                              (interleave centroids
+                                          (repeat (count centroids) [])))
+                       coll)]
+    (if (> iterations 1)
+      (k-means (apply concat (vals result))
+               (map (fn [[centroid values]] (avg values)) result)
+               distance-fn
+               avg-fn
+               (dec iterations))
+      result)))
+
+(defn cluster
+  [colors]
+  (let [sorted-colors (sort-by (partial color-diff [0 0 0]) colors)]
+    (k-means colors
+             [(first sorted-colors) (last sorted-colors)]
+             color-diff
+             avg
+             1)))
--- src/crokinole_score_keeper/core.clj
+++ src/crokinole_score_keeper/core.clj
@@ -1,5 +1,7 @@
 (ns crokinole-score-keeper.core
-  (:require [clojure.pprint :as pprint])
+  (:require [clojure.pprint :as pprint]
+            [clojure.set :refer [map-invert]]
+            [crokinole-score-keeper.color :as color])
   (:import [org.opencv.core Core Mat Size]
            [org.opencv.imgproc Imgproc]
            [org.opencv.videoio VideoCapture VideoWriter Videoio]
@@ -7,54 +9,49 @@
 
 (clojure.lang.RT/loadLibrary Core/NATIVE_LIBRARY_NAME)
 
-#_(VideoWriter. "resources/out.mp4"
-                (VideoWriter/fourcc \H \2 \6 \4)
-                (.get video-in Videoio/CAP_PROP_FPS)
-                (Size. (.get video-in Videoio/CAP_PROP_FRAME_WIDTH)
-                       (.get video-in Videoio/CAP_PROP_FRAME_HEIGHT))
-                true)
-
-(defn center-circle
-  "Find the circle closest to the center of the width and height.
-  The result should be the 20 point ring on the board."
-  [width height circles]
-  (some->> circles
-           (sort-by (fn [[x y _]]
-                      (let [rx (- x (/ width 2))
-                            ry (- y (/ height 2))]
-                        (+ (Math/abs rx) (Math/abs ry)))))
-           first))
+(defonce frames (atom 0))
 
 (defn detect-circles
   [frame]
   (let [grayscale (volatile! (Mat.))
+        blur (volatile! (Mat.))
         discs (volatile! (Mat.))
-        ring5 (volatile! (Mat.))
-        ring10 (volatile! (Mat.))
         ring15 (volatile! (Mat.))
         _ (Imgproc/cvtColor frame @grayscale Imgproc/COLOR_BGR2GRAY)
+        _ (Imgproc/GaussianBlur frame @blur (Size. 5 5) 0 0)
         _ (Imgproc/HoughCircles @grayscale @discs
                                 Imgproc/CV_HOUGH_GRADIENT
                                 1 20 50 30
                                 (/ (.width frame) 128)
                                 (/ (.width frame) 64))
-        discs (for [x (range (.cols @discs))]
-                (vec (.get @discs 0 x)))
-        [r20x r20y r20r] (center-circle (.width frame) (.height frame) discs)
+        normalized-discs (->> (for [x (range (.cols @discs))]
+                                (vec (.get @discs 0 x)))
+                              (mapv (fn [[x y radius]]
+                                      (let [[b g r] (vec (.get @blur y x))]
+                                        [x y radius [r g b]]))))
+        colors-lookup (->> (reduce (fn [accl [centroid colors]]
+                                     (conj accl (reduce #(conj %1 %2 centroid)
+                                                        []
+                                                        colors)))
+                                   []
+                                   (color/cluster (mapv last normalized-discs)))
+                           (apply concat)
+                           (apply hash-map))
+        r20r (/ (reduce + (map (fn [disc] (nth disc 2)) normalized-discs))
+                (count normalized-discs))
         _ (Imgproc/HoughCircles @grayscale @ring15
                                 Imgproc/CV_HOUGH_GRADIENT
                                 1 100 15 50 (* r20r 6) (* r20r 8))
-        _ (Imgproc/HoughCircles @grayscale @ring10
-                                Imgproc/CV_HOUGH_GRADIENT
-                                1 100 15 50 (* r20r 12) (* r20r 14))
-        _ (Imgproc/HoughCircles @grayscale @ring5
-                                Imgproc/CV_HOUGH_GRADIENT
-                                1 100 15 50 (* r20r 21) (* r20r 22))]
-    {:ring5 (vec (.get @ring5 0 0))
-     :ring10 (vec (.get @ring10 0 0))
-     :ring15 (vec (.get @ring15 0 0))
-     :ring20 [r20x r20y r20r]
-     :discs (remove #(= % [r20x r20y r20r]) discs)}))
+        ring15 (vec (.get @ring15 0 0))
+        ring15-coords (-> ring15 drop-last vec)]
+    {:dimensions [(.width frame) (.height frame)]
+     :ring5 (conj ring15-coords (* (last ring15) 3))
+     :ring10 (conj ring15-coords (* (last ring15) 2))
+     :ring15 ring15
+     :ring20 (conj ring15-coords
+                   (/ (last ring15) (* (/ 4 5.5) 8)))
+     :discs normalized-discs
+     :colors-lookup colors-lookup}))
 
 (defn analyze-image
   ([]
@@ -66,13 +63,22 @@
   ([]
    (analyze-video "resources/crokinole.mp4"))
   ([filename]
+   (reset! frames 0)
    (let [buffer (volatile! (Mat.))
          video-in (VideoCapture. filename)]
      (while (.read video-in @buffer)
-       (detect-circles @buffer)
-       #_(.write video-out @buffer))
+       (swap! frames inc)
+       (detect-circles @buffer))
      (.release video-in))))
 
 (defn -main
   [& args]
-  (analyze-video))
+  (pprint/pprint (reduce (fn [accl filename]
+                           (assoc accl filename
+                                  (analyze-image filename)))
+                         {}
+                         ["resources/sample1.png"
+                          "resources/sample2.png"
+                          "resources/sample3.png"
+                          "resources/sample3_nocenter.png"
+                          "resources/crokinole.jpg"])))
